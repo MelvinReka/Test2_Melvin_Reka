37bc9259fbe8d31236efdd121f7097c97c126774
Melvin Reka <02rekmel@rgtfo-me.it>
2020-04-17 09:53:55 +0200

    Revert "Einlesen Binärzahl"
    This reverts commit 7fc10364e33a0693aa49b1439206db4d98d4fc18.

--- Test2_Melvin_Reka/Test2_Melvin_Reka/Program.cs
+++ Test2_Melvin_Reka/Test2_Melvin_Reka/Program.cs
@@ -9,43 +9,21 @@ namespace Test2_Melvin_Reka
     {
         static void Main(string[] args)
         {
-            
-            Titel("Dies ist ein Test",ConsoleColor.Red);
-            string binzahl = Binaerzahleinlesen();
+            Titel("Dies ist ein Test");
             Console.ReadKey();
         }
-        static void Titel(string titel, ConsoleColor textfarbe)
-        {
-            
-            int breite = Console.WindowWidth;
-            int textlen = titel.Length;
-            ConsoleColor aktTextFarbe = Console.ForegroundColor;
 
-            Console.Clear();
+        static void Titel(string titel)
+        {
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("*");
-            }
-            for (int i = 0; i < (breite-textlen/2); i++)
-            {
-                Console.Write(" ");
             }
             Console.WriteLine(titel);
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("*");
             }
-            
-        }
-        /// <summary>
-        /// Einlesen einer Binärzahl
-        /// </summary>
-        /// <returns>String mit nur Nullen und Einsen</returns>
-        static string Binaerzahleinlesen()
-        {
-            Console.WriteLine("Bitte Binärzahl eingeben (0/1): ");
-            string input = Console.ReadLine();
-            return input;
         }
     }
 }
